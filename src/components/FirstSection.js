0d454780a3ede05d9c9f0cb9b92c411ce30355a7
Ajinkya <ap@gmail.com>
2023-11-08 13:59:15 +0530

    First

---
+++ src/components/FirstSection.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../components/css/firstSection.css"
+
+const FirstSection = ()=>{
+    return(
+        <div className="fContent">
+            <div className="appName"></div>
+            <div className="extraSpace"></div>
+            <div className="userDropdown"></div>
+        </div>
+    )
+}
+
+export default FirstSection;
---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../components/css/main.css"
+import FirstSection from "../components/FirstSection"
+
+const Main = ()=>{
+    return(
+        <div className="content">
+
+            <div className="firstSection"><FirstSection/></div>
+            <div className="secondSection"></div>
+            <div className="thirdSection"></div>
+
+        </div>
+
+       
+    );
+}
+
+export default Main;
