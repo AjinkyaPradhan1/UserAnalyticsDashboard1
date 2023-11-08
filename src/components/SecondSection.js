1bebcaed78103a0fe540373130a98b8523985c9c
Ajinkya <ap@gmail.com>
2023-11-08 18:56:05 +0530

    Visits Try

--- src/components/SecondSection.js
+++ src/components/SecondSection.js
@@ -3,6 +3,12 @@ import "../components/css/secondSection.css"
 
 const SecondSection = ()=>{
 
+    const webVisits = () =>{
+        return(
+            fetch('https://api.countapi.xyz/update/value/?amount=1')
+            .then((res)=>res.json())
+        )
+    }
     
     return(
         <div className="sContent">
@@ -18,7 +24,7 @@ const SecondSection = ()=>{
                         <div className="sContent_left_data_2">
                             <div className="sContent_left_data_2_content">
                                 <div className="sContent_left_data_2_content1">Total Visits</div>
-                                <div className="sContent_left_data_2_content2"></div>
+                                <div className="sContent_left_data_2_content2">{webVisits}</div>
                             </div>
                         </div>
 
