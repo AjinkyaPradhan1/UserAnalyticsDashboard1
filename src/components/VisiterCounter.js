23430dff8a9d42524bf00875246822bc17040f94
Ajinkya <ap@gmail.com>
2023-11-08 19:37:41 +0530

    Visits Try

--- src/components/VisiterCounter.js
+++ src/components/VisiterCounter.js
@@ -9,15 +9,11 @@ const VisitorCounter = () => {
     const initialCount = Number(savedCount) || 0;
     setCount(initialCount+1);
     
-    // Increment the count and save it to localStorage when the component unmounts or the page is closed
-    return () => {
-      localStorage.setItem('visitorCount', count + 1);
-    };
   }, []);
 
   return (
     <div>
-      {setCount}
+      {count}
     </div>
   );
 };
