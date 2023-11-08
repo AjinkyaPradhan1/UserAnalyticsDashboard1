bba7683cd25e9ea819f5b2b3690f36ee2dcf43c6
Ajinkya <ap@gmail.com>
2023-11-08 14:21:24 +0530

    second

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -4,9 +4,17 @@ import "../components/css/firstSection.css"
 const FirstSection = ()=>{
     return(
         <div className="fContent">
-            <div className="appName"></div>
+            <div className="appName">
+                <div className="appNametop">User Analytics</div>
+                <div className="appNamebottom">Lorem ipsum dolor sit consectetur</div>
+            </div>
             <div className="extraSpace"></div>
-            <div className="userDropdown"></div>
+            <div className="userDropdown">
+                <div className="userDropdownContent">
+                    <div className="Text">User</div>
+                    <div className="symbol"></div>
+                </div>
+            </div>
         </div>
     )
 }
