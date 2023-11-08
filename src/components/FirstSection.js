4be0ace2c9714c686754681bf2af430c42a81690
Ajinkya <ap@gmail.com>
2023-11-08 16:48:27 +0530

    First Section Complete

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -12,7 +12,11 @@ const FirstSection = ()=>{
             <div className="userDropdown">
                 <div className="userDropdownContent">
                     <div className="text">User</div>
-                    <div className="symbol"></div>
+                    <div className="symbol">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="7" viewBox="0 0 12 7" fill="none">
+                        <path d="M1 1L6 6L11 1" stroke="#747E9A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
                 </div>
             </div>
         </div>
