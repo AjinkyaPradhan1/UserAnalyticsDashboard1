68a278692b26db6f83595b8460ac1aaf8d6949f7
Ajinkya <ap@gmail.com>
2023-11-08 20:49:07 +0530

    Third Section

--- src/components/ThirdSection.js
+++ src/components/ThirdSection.js
@@ -4,7 +4,9 @@ import "../components/css/thirdSection.css"
 const ThirdSection = ()=>{
     return(
         <div className="tContent">
-            <div className="thirdFirst"></div>
+            <div className="thirdFirst">
+                
+            </div>
             <div className="thirdSecond"></div>
             <div className="thirdThird"></div>
         </div>
